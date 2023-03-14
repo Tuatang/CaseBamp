981f9f4b4fbbec1df9aee11aa06fe991d75c67e8
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-03-14 17:28:15 +0700

    <feat> :add class Organization

--- main.py
+++ main.py
@@ -28,5 +28,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+from organization import Organization
+from user import User
+
+def main() :
+    organization_1 = Organization("KMITL")
+    user_1 = User("koonmartza123@gmail.com", "thanathat p.", "092xxxxxxx")
+    user_1.create_project("Testing", "test", "26-03-2023", "30-03-2023")
+    organization_1.show_projects()
 
 #edit
+if __name__ == "__main__" :
+    main()
