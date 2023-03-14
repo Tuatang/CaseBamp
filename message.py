0b3024361ba4423d9ffa4089e9021d244d99bcf6
Tuatang <makv998@gmail.com>
2023-03-14 17:17:15 +0700

    <feat> :added message.py

---
+++ message.py
@@ -0,0 +1,15 @@
+
+class Message:
+	id = 1
+	def __init__(self, title, content, timestamp, added_by, notify_type, received_users):
+		self.__message_title = title
+		self.__message_id = id
+		self.__content = content
+		self.__timestamp = timestamp
+		self.__added_by = added_by
+		self.__notify_type = notify_type
+		self.__received_users = received_users
+		id += 1 
+	def method(self):
+		pass
+
