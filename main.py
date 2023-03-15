8ff8713f17b58144e8cf8d239221186ac3e009ec
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-03-15 17:38:53 +0700

    <feat> :add method create_project class Org

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from organization import Organization
-from user import User
 
 def main() :
     organization_1 = Organization("KMITL")
-    user_1 = User("koonmartza123@gmail.com", "thanathat p.", "092xxxxxxx")
-    user_1.create_project("Testing", "test", "26-03-2023", "30-03-2023")
+    organization_1.create_project("Sleep", "Relax", "15-03-2023", "20-03-2023")
     organization_1.show_projects()
 
 if __name__ == "__main__" :
--- organization.py
+++ organization.py
@@ -1,15 +1,19 @@
 from random import randint
-from user import User
+from project import Project
 
 class Organization :
-    def __init__(self, organization_name : str, organization_logo : str = "default_logo_image.png") :
+    def __init__(self, organization_name) :
         self.__organization_id = randint(10000000, 100000000);
         self.__organization_name = organization_name
-        self.__organization_logo = organization_logo
+        # self.__organization_logo = organization_logo
         self.__organization_member = []
         self.__notifications = []
         self.__projects = []
         self.__activity_thread = []
+
+    def create_project(self, name, description, start_date, end_date) :
+        name = Project(name, description, start_date, end_date)
+        self.__projects.append(name)
 
     def show_projects(self) :
         if self.__projects is not None:
