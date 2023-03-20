9f4f95070c5dc5579f613a522af9271fd461587d
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-03-21 00:02:49 +0700

    <feat> :add method show_activity

--- activity.py
+++ activity.py
@@ -12,8 +12,16 @@ class ActivityLog :
         if self.__activity_list != None:
             return self.__activity_list
         
+    def show_activity(self) :
+        if self.__activity_list is not None:
+            for a in self.__activity_list :
+                print(a)
+        
 class Activity :
     def __init__(self, detail, timestamp, users) :
         self.__detail = detail
         self.__timestamp = timestamp
         self.__accomplished_by = users
+
+    def __str__(self) :
+        return 'Activity: ' + str(self.__detail) + ' Timestamp: ' + str(self.__timestamp) + ' Accomplished by: ' + str(self.__accomplished_by)
