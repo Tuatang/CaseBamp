c1ae65e9f64619bafe5c02d35e9cd886e0d3f939
Nitcharat <pondaeorz008y@gmail.com>
2023-03-20 23:34:48 +0700

    <feat> :added messageandchat.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from event import Event
+from messageandchat import *
 from datetime import datetime
 
 def main():
-    # Create an event
-    event_1 = Event(datetime(2015, 11, 5, 10, 0), datetime(2015, 11, 5, 11, 0), "Python Class", "Learn Python")
-    print(event_1)
+    message1 = Message("Message 1", Content("Details"), datetime.now())
+    message2 = Message("Message 2", Content("Details"), datetime.now())
+
+    message_boad = MessageBoard()
+    message_boad.save_message(message1)
+    message_boad.save_message(message2)
+
+    chat1 = Chat(Content("Details"), datetime.now(), "User 1")
+    chat2 = Chat(Content("Details"), datetime.now(), "User 2")
+
+    chat_history = ChatHistory([chat1, chat2], ["User 1", "User 2"])
+
+    event1 = Event("Event 1", Content("Details"), datetime.now(), datetime.now(), True, ["User 1", "User 2"])
+    event2 = Event("Event 2", Content("Details"), datetime.now(), datetime.now(), True, ["User 1", "User 2"])
+
+
     
 if __name__ == "__main__":
     main()
+
+#edit
---
+++ useraccount.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, email, name, phone, organization, is_admin = False, is_owner = False):
+        self.__email = email
+        self.__name = name
+        self.__phone = phone
+        self.__organization = organization
+        self.__is_admin = is_admin
+        self.__is_owner = is_owner
+        self.__list_of_private_chat = []
+        self.__list_of_notification = []
+
+    def set_owner_role(self, is_owner):
+        self.__is_owner = is_owner
+    def set_admin_role(self, is_admin):
+        self.__is_admin = is_admin
+    def  check_user_for_chat_history(self, user, chat_history):
+        pass
+    def search_chat_history(self, sender, receiver):
+        pass
+    def get_chat_history(self):
+        pass
+    def get_notification(self):
+        pass
+    def edit_profile(self, name, phone):
+        pass
+    def request_chat(self, chat_type: str):
+        pass
+    def get_private_chat(self):
+        pass
+    
