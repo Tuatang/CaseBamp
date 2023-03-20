10fcd6a97a5a1ea29c4aeb36649feba7f3179872
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-03-20 23:51:19 +0700

    <feat> :test class Organization

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ from organization import Organization
 def main() :
     organization_1 = Organization("KMITL")
     organization_1.create_project("Sleep", "Relax", "15-03-2023", "20-03-2023")
+    organization_1.get_project()
     organization_1.show_projects()
 
 #edit
