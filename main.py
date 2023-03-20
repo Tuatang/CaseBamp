baeb9a87e7edee48d5255e08c0c6772a4a0b62cc
Nitcharat <pondaeorz008y@gmail.com>
2023-03-20 23:51:57 +0700

    <feat> :edited messageandchat.py and main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from messageandchat import *
 from datetime import datetime
+from useraccount import *
 
 def main():
+    account = Account("pond", "pond1234")
+    user1 = User("65010539@kmitl.ac.th", "Pond", "0812345678", "KMITL", False, False)
+    user1.edit_profile("Pond", "0812345678")
+    noti1 = Notification("Notification 1", Content("Details"), datetime.now(), "User 1", ["User 1", "User 2"])
+    noti2 = Notification("Notification 2", Content("Details"), datetime.now(), "User 2", ["User 1", "User 2"])
+    noti1.send_notification(user1)
+    noti2.send_notification(user1)
+
     message1 = Message("Message 1", Content("Details"), datetime.now())
     message2 = Message("Message 2", Content("Details"), datetime.now())
 
@@ -17,8 +26,6 @@ def main():
     event1 = Event("Event 1", Content("Details"), datetime.now(), datetime.now(), True, ["User 1", "User 2"])
     event2 = Event("Event 2", Content("Details"), datetime.now(), datetime.now(), True, ["User 1", "User 2"])
 
-
-    
 if __name__ == "__main__":
     main()
 
--- useraccount.py
+++ useraccount.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from random import randint
 class User:
     def __init__(self, email, name, phone, organization, is_admin = False, is_owner = False):
         self.__email = email
@@ -26,4 +28,25 @@ class User:
     def request_chat(self, chat_type: str):
         pass
     def get_private_chat(self):
+        pass
+
+class Account:
+    def __init__(self, account_email, account_password):
+        self.__account_email = account_email
+        self.__account_password = account_password
+        self.__account_users = []
+    
+    def log_out(self):
+        pass
+
+class Notification:
+    def __init__(self, title, details, timestamp:datetime, sender, receivers:list):
+        self.__title = title
+        self.__id = randint(100000, 1000000)
+        self.__details = details
+        self.__timestamp = timestamp
+        self.__sender = sender
+        self.__receivers = receivers
+
+    def send_notification(self, user:User):
         pass
